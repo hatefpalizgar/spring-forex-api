b121b314f777dd84177ce08bbfe1994ac81bf51a
hatef palizgar <hatef.palizgar@gmail.com>
2020-04-20 01:38:11 +0300

    add caching mechanism

--- src/main/java/com/gamesys/hatef/SpringForexApiApplication.java
+++ src/main/java/com/gamesys/hatef/SpringForexApiApplication.java
@@ -2,12 +2,23 @@ package com.gamesys.hatef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
 
 
+@EnableCaching
 @SpringBootApplication
 public class SpringForexApiApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(SpringForexApiApplication.class, args);
     }
+    
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("candles");
+    }
 }
+
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE candles
+(
+    datetime VARCHAR(100), -- It is much more simpler to get dateTime as string and then convert it for further usage
+    open     double,
+    high     double,
+    low      double,
+    close    double
+);
