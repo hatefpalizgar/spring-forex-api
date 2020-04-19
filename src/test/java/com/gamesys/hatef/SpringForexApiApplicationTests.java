19d02f0ef30c4644938381d015739ade0a0c3a71
hatef palizgar <hatef.palizgar@gmail.com>
2020-04-20 01:25:13 +0300

    initial commit

---
+++ src/test/java/com/gamesys/hatef/SpringForexApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesys.hatef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringForexApiApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+}
